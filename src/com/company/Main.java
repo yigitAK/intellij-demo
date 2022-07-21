324dc872730c1b06699dd7cdc65f5f18e19e8a1c
yigitAK <y.ak@ebebek.com>
2022-07-21 15:06:49 +0300

    Hello user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String user = input.nextLine();
+
+	    System.out.println("Hello World and " + user);
     }
 }
